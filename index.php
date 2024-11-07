cde008f6919c521c402367cdc437132610429ec2
Jesiee <solante.jescel72@gmail.com>
2024-11-06 23:49:45 -0800

    Update index.php

--- index.php
+++ index.php
@@ -42,7 +42,8 @@
 
 <!-- Submit Button -->
 <div>
-    <button type="submit" class="form-control">Submit</button>
+   
+ <button type="submit" style="width: 100%; padding: 10px; background-color: #4caf50; color: white; font-size: 16px; font-weight: bold; border: none; border-radius: 4px; cursor: pointer; transition: background-color 0.3s ease;">Submit</button>
 </div>
 
 
